eb91e606a90d61d5470b38e5ff8e566df7136aab
losrobbos <losrobbos@web.de>
2022-11-12 07:53:29 +0100

    Updated comments

--- 03-move-it-baby/script.js
+++ 03-move-it-baby/script.js
@@ -9,30 +9,30 @@
  *   - take old position
  *   - extract position pixels as number
  *   - add 20 pixel on top
- *   - add px to new number 
+ *   - add "px" string to new position
+ *   - assign pixels string to position
  */
 
 const elBall = document.querySelector(".ball");
 const elMain = document.querySelector("main");
 
-const ballSpeedInPx = 20
+const ballSpeedInPx = 20;
 
-
-// keyboard input ???
+// handle keyboard input
 window.addEventListener("keydown", (e) => {
-
+  
   // determine current position in pixel!
-  const ballStyle = getComputedStyle(elBall)
+  const ballStyle = getComputedStyle(elBall);
   let leftInPx = ballStyle.left;
-  let bottomInPx = ballStyle.bottom
-  let topInPx = ballStyle.top
-  let rightInPx = ballStyle.right
+  let bottomInPx = ballStyle.bottom;
+  let topInPx = ballStyle.top;
+  let rightInPx = ballStyle.right;
 
   switch (e.key) {
     case "ArrowLeft":
       console.log("Moving left....");
 
-      // extract pixel number and subtract pixels from left 
+      // extract pixel number and subtract pixels from left
       leftInPx = parseInt(leftInPx) - ballSpeedInPx;
       rightInPx = parseInt(rightInPx) + ballSpeedInPx;
 
@@ -42,30 +42,31 @@ window.addEventListener("keydown", (e) => {
 
       // extract pixel number and add pixels to left
       leftInPx = parseInt(leftInPx) + ballSpeedInPx;
-      rightInPx = parseInt(rightInPx) - ballSpeedInPx
+      rightInPx = parseInt(rightInPx) - ballSpeedInPx;
 
       break;
     case "ArrowUp":
       console.log("Moving up...");
-      
+
       // extract pixel number and add pixels to bottom
-      bottomInPx = parseInt(bottomInPx) + ballSpeedInPx
+      bottomInPx = parseInt(bottomInPx) + ballSpeedInPx;
       topInPx = parseInt(topInPx) - ballSpeedInPx;
 
       break;
     case "ArrowDown":
-      console.log("Moving down...")
-      
+      console.log("Moving down...");
+
       // extract pixel number and subtracting pixels to bottom
       bottomInPx = parseInt(bottomInPx) - ballSpeedInPx;
-      topInPx = parseInt(topInPx) + ballSpeedInPx
+      topInPx = parseInt(topInPx) + ballSpeedInPx;
       break;
   }
 
-  // stop right at => arena width - ball width
-  if( leftInPx >= 0 && rightInPx >= 0 )
+  // stop if ball exceed left or right border of container
+  if (leftInPx >= 0 && rightInPx >= 0) 
     elBall.style.left = `${leftInPx}px`;
 
-  if(bottomInPx >= 0 && topInPx >= 0 )
-    elBall.style.bottom = `${bottomInPx}px`
+  // stop if ball exceeds top or bottom border of container
+  if (bottomInPx >= 0 && topInPx >= 0) 
+    elBall.style.bottom = `${bottomInPx}px`;
 });

af4e5745e83441529c4518e31ffe76240f8a6f99
losrobbos <losrobbos@web.de>
2022-11-12 07:50:15 +0100

    Move ball in arena sample - cleanup

--- 03-move-it-baby/script.js
+++ 03-move-it-baby/script.js
@@ -17,56 +17,55 @@ const elMain = document.querySelector("main");
 
 const ballSpeedInPx = 20
 
-console.log(elMain)
-
-const ballWidthInPx = parseInt( getComputedStyle(elBall).width )
-const arenaWidthInPx = parseInt( getComputedStyle(elMain).width )
-const arenaHeightInPx = parseInt( getComputedStyle(elMain).height)
-
-console.log({ arenaWidthInPx, arenaHeightInPx, ballWidthInPx })
 
 // keyboard input ???
 window.addEventListener("keydown", (e) => {
 
   // determine current position in pixel!
-  let newLeftInPixel = getComputedStyle(elBall).left;
-  let newBottomInPixel = getComputedStyle(elBall).bottom
+  const ballStyle = getComputedStyle(elBall)
+  let leftInPx = ballStyle.left;
+  let bottomInPx = ballStyle.bottom
+  let topInPx = ballStyle.top
+  let rightInPx = ballStyle.right
 
   switch (e.key) {
     case "ArrowLeft":
       console.log("Moving left....");
 
       // extract pixel number and subtract pixels from left 
-      newLeftInPixel = parseInt(newLeftInPixel) - ballSpeedInPx;
+      leftInPx = parseInt(leftInPx) - ballSpeedInPx;
+      rightInPx = parseInt(rightInPx) + ballSpeedInPx;
 
       break;
     case "ArrowRight":
       console.log("Moving right....");
 
       // extract pixel number and add pixels to left
-      newLeftInPixel = parseInt(newLeftInPixel) + ballSpeedInPx;
+      leftInPx = parseInt(leftInPx) + ballSpeedInPx;
+      rightInPx = parseInt(rightInPx) - ballSpeedInPx
 
       break;
     case "ArrowUp":
-      console.log("Moving uo...");
-
+      console.log("Moving up...");
+      
       // extract pixel number and add pixels to bottom
-      newBottomInPixel = parseInt(newBottomInPixel) + ballSpeedInPx
+      bottomInPx = parseInt(bottomInPx) + ballSpeedInPx
+      topInPx = parseInt(topInPx) - ballSpeedInPx;
 
       break;
     case "ArrowDown":
+      console.log("Moving down...")
+      
       // extract pixel number and subtracting pixels to bottom
-
-      newBottomInPixel = parseInt(newBottomInPixel) - ballSpeedInPx;
+      bottomInPx = parseInt(bottomInPx) - ballSpeedInPx;
+      topInPx = parseInt(topInPx) + ballSpeedInPx
       break;
   }
 
-  // console.log({ left: newLeftInPixel, bottom: newBottomInPixel })
-
   // stop right at => arena width - ball width
-  if(newLeftInPixel >= 0 &&  newLeftInPixel <= (arenaWidthInPx-ballWidthInPx) )
-    elBall.style.left = `${newLeftInPixel}px`;
+  if( leftInPx >= 0 && rightInPx >= 0 )
+    elBall.style.left = `${leftInPx}px`;
 
-  if(newBottomInPixel >= 0 && newBottomInPixel <= (arenaHeightInPx-ballWidthInPx))
-    elBall.style.bottom = `${newBottomInPixel}px`
+  if(bottomInPx >= 0 && topInPx >= 0 )
+    elBall.style.bottom = `${bottomInPx}px`
 });

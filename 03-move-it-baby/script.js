6617abc1fb6f30ed524a0b956e9e02db7fd150ef
losrobbos <losrobbos@web.de>
2022-11-16 19:38:52 +0100

    Moving ball sample - cleanup

--- 03-move-it-baby/script.js
+++ 03-move-it-baby/script.js
@@ -22,51 +22,53 @@ const ballSpeedInPx = 20;
 window.addEventListener("keydown", (e) => {
   
   // determine current position in pixel!
-  const ballStyle = getComputedStyle(elBall);
-  let leftInPx = ballStyle.left;
-  let bottomInPx = ballStyle.bottom;
-  let topInPx = ballStyle.top;
-  let rightInPx = ballStyle.right;
+  let { left, right, top, bottom } = getComputedStyle(elBall);
+  
+  // convert position strings to pure numbers
+  left = parseInt(left);
+  right = parseInt(right);
+  top = parseInt(top);
+  bottom = parseInt(bottom);
 
   switch (e.key) {
     case "ArrowLeft":
       console.log("Moving left....");
 
       // extract pixel number and subtract pixels from left
-      leftInPx = parseInt(leftInPx) - ballSpeedInPx;
-      rightInPx = parseInt(rightInPx) + ballSpeedInPx;
+      left -= ballSpeedInPx;
+      right += ballSpeedInPx;
 
       break;
     case "ArrowRight":
       console.log("Moving right....");
 
       // extract pixel number and add pixels to left
-      leftInPx = parseInt(leftInPx) + ballSpeedInPx;
-      rightInPx = parseInt(rightInPx) - ballSpeedInPx;
+      left += ballSpeedInPx;
+      right -= ballSpeedInPx;
 
       break;
     case "ArrowUp":
       console.log("Moving up...");
 
       // extract pixel number and add pixels to bottom
-      bottomInPx = parseInt(bottomInPx) + ballSpeedInPx;
-      topInPx = parseInt(topInPx) - ballSpeedInPx;
+      bottom += ballSpeedInPx;
+      top -= ballSpeedInPx;
 
       break;
     case "ArrowDown":
       console.log("Moving down...");
 
       // extract pixel number and subtracting pixels to bottom
-      bottomInPx = parseInt(bottomInPx) - ballSpeedInPx;
-      topInPx = parseInt(topInPx) + ballSpeedInPx;
+      bottom -= ballSpeedInPx;
+      top += ballSpeedInPx;
       break;
   }
 
   // stop if ball exceed left or right border of container
-  if (leftInPx >= 0 && rightInPx >= 0) 
-    elBall.style.left = `${leftInPx}px`;
+  if (left >= 0 && right >= 0) 
+    elBall.style.left = `${left}px`;
 
   // stop if ball exceeds top or bottom border of container
-  if (bottomInPx >= 0 && topInPx >= 0) 
-    elBall.style.bottom = `${bottomInPx}px`;
+  if (bottom >= 0 && top >= 0) 
+    elBall.style.bottom = `${bottom}px`;
 });
